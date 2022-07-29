7e55dbecd734f7517f8ee6dc3d6702dd671083b3
Snn <15135068581@163.com>
2022-07-29 11:32:36 +0800

    jzz修改app

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,30 @@
   <div id="app">
     <router-view />
 
-    <van-tabbar route>
-      <van-tabbar-item replace to="/Personnel" icon="home-o"
+    <van-tabbar routes v-model="active">
+      <van-tabbar-item  to="/Personnel" icon="home-o"
         >办公</van-tabbar-item
       >
-      <van-tabbar-item replace to="/schedule" icon="search"
+      <van-tabbar-item  to="/schedule" icon="search"
         >日程</van-tabbar-item
       >
-      <van-tabbar-item replace to="/ipa" icon="home-o">通讯录</van-tabbar-item>
-      <van-tabbar-item replace to="/my" icon="search">我的</van-tabbar-item>
+      <van-tabbar-item  to="/ipa" icon="home-o">通讯录</van-tabbar-item>
+      <van-tabbar-item  to="/my" icon="search">我的</van-tabbar-item>
     </van-tabbar>
+
+    
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
+
 
 <style></style>
